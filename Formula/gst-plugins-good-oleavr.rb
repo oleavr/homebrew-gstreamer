deb3bccb7f227db3d953c1f47e07669fa89f057a
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-11-12 02:15:49 +0100

    Add formula for gst-plugins-good

---
+++ Formula/gst-plugins-good-oleavr.rb
@@ -0,0 +1,81 @@
+# Based on the formula from homebrew-core.
+
+class GstPluginsGoodOleavr < Formula
+  desc "GStreamer plugins (well-supported, under the LGPL)"
+  homepage "https://gstreamer.freedesktop.org/"
+  url "git://github.com/oleavr/gst-plugins-good.git"
+  version "1.12.0.r545.g770bb07f3"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "check"
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gst-plugins-base-oleavr"
+  depends_on "libsoup"
+
+  depends_on :x11 => :optional
+
+  # Dependencies based on the intersection of
+  # https://cgit.freedesktop.org/gstreamer/gst-plugins-good/tree/REQUIREMENTS
+  # and Homebrew formulae.
+  depends_on "jpeg" => :recommended
+  depends_on "orc" => :recommended
+  depends_on "gdk-pixbuf" => :optional
+  depends_on "aalib" => :optional
+  depends_on "cairo" => :optional
+  depends_on "flac" => [:optional, "with-libogg"]
+  depends_on "libcaca" => :optional
+  depends_on "libdv" => :optional
+  depends_on "libpng" => :optional
+  depends_on "libshout" => :optional
+  depends_on "speex" => :optional
+  depends_on "taglib" => :optional
+
+  depends_on "libvpx" => :optional
+  depends_on "pulseaudio" => :optional
+  depends_on "jack" => :optional
+
+  depends_on "libogg" if build.with? "flac"
+
+  conflicts_with "gst-plugins-good"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-gtk-doc
+      --disable-goom
+      --with-default-videosink=ximagesink
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    if build.with? "x11"
+      args << "--with-x"
+    else
+      args << "--disable-x"
+    end
+
+    # This plugin causes hangs on Snow Leopard (and possibly other versions?)
+    # Upstream says it hasn't "been actively tested in a long time";
+    # successor is glimagesink (in gst-plugins-bad).
+    # https://bugzilla.gnome.org/show_bug.cgi?id=756918
+    args << "--disable-osx_video" if MacOS.version == :snow_leopard
+
+    ENV["NOCONFIGURE"] = "yes"
+    system "./autogen.sh"
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    gst = Formula["gstreamer"].opt_bin/"gst-inspect-1.0"
+    output = shell_output("#{gst} --plugin cairo")
+    assert_match version.to_s, output
+  end
+end

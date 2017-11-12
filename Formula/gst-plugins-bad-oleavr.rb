14b756392679cf90aaab0ad0d9141c5d0f85f704
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-11-12 02:25:51 +0100

    Add formula for gst-plugins-bad

---
+++ Formula/gst-plugins-bad-oleavr.rb
@@ -0,0 +1,63 @@
+# Based on the formula from homebrew-core.
+
+class GstPluginsBadOleavr < Formula
+  desc "GStreamer plugins less supported, not fully tested"
+  homepage "https://gstreamer.freedesktop.org/"
+  url "git://github.com/oleavr/gst-plugins-bad.git"
+  version "1.12.0.r579.gb9634580c"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gst-plugins-base-oleavr"
+  depends_on "openssl"
+  depends_on "jpeg" => :recommended
+  depends_on "orc" => :recommended
+  depends_on "dirac" => :optional
+  depends_on "faac" => :optional
+  depends_on "faad2" => :optional
+  depends_on "fdk-aac" => :optional
+  depends_on "gnutls" => :optional
+  depends_on "gtk+3" => :optional
+  depends_on "libdvdread" => :optional
+  depends_on "libexif" => :optional
+  depends_on "libmms" => :optional
+  depends_on "opencv@2" => :optional
+  depends_on "opus" => :optional
+  depends_on "rtmpdump" => :optional
+  depends_on "schroedinger" => :optional
+  depends_on "sound-touch" => :optional
+  depends_on "srtp@1.6" => :optional
+  depends_on "libvo-aacenc" => :optional
+
+  conflicts_with "gst-plugins-bad"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-yadif
+      --disable-examples
+      --disable-debug
+      --disable-dependency-tracking
+    ]
+
+    args << "--with-gtk=3.0" if build.with? "gtk+3"
+
+    # autogen is invoked in "stable" build because we patch configure.ac
+    ENV["NOCONFIGURE"] = "yes"
+    system "./autogen.sh"
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    gst = Formula["gstreamer"].opt_bin/"gst-inspect-1.0"
+    output = shell_output("#{gst} --plugin dvbsuboverlay")
+    assert_match version.to_s, output
+  end
+end

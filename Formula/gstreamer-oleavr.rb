c9fc6bbbb545d889a84ab8ac382d45e7d175553a
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-11-12 01:53:56 +0100

    Revert "Switch to Meson"
    Introspection generation fails. I'll leave this for another day.
    
    This reverts commit bbee95cab1e89fd6ac1e0e1399d57b60d035bf6e.

--- Formula/gstreamer-oleavr.rb
+++ Formula/gstreamer-oleavr.rb
@@ -6,8 +6,9 @@ class GstreamerOleavr < Formula
   url "git://github.com/oleavr/gstreamer.git"
   version "1.12.0.r189.ge1f039b7f"
 
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
   depends_on "gobject-introspection"
@@ -20,26 +21,29 @@ class GstreamerOleavr < Formula
   def install
     args = %W[
       --prefix=#{prefix}
-      --buildtype release
-      --strip
-      -Ddisable_gst_debug=true
-      -Ddisable_gtkdoc=true
-      -Ddisable_introspection=false
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-gtk-doc
+      --enable-introspection=yes
     ]
+
+    ENV["NOCONFIGURE"] = "yes"
+    system "./autogen.sh"
 
     # Ban trying to chown to root.
     # https://bugzilla.gnome.org/show_bug.cgi?id=750367
-    args << "-Dwith-ptp-helper-permissions=none"
+    args << "--with-ptp-helper-permissions=none"
 
     # Look for plugins in HOMEBREW_PREFIX/lib/gstreamer-1.0 instead of
     # HOMEBREW_PREFIX/Cellar/gstreamer/1.0/lib/gstreamer-1.0, so we'll find
     # plugins installed by other packages without setting GST_PLUGIN_PATH in
     # the environment.
-    inreplace "meson.build", /cdata\.set_quoted\('PLUGINDIR', [^\n]+/,
-      "cdata.set_quoted('PLUGINDIR', '#{HOMEBREW_PREFIX}/lib/gstreamer-1.0')"
+    inreplace "configure", 'PLUGINDIR="$full_var"',
+      "PLUGINDIR=\"#{HOMEBREW_PREFIX}/lib/gstreamer-1.0\""
 
-    system "meson", *args, "_build", "."
-    system "ninja", "-C", "_build", "install"
+    system "./configure", *args
+    system "make"
+    system "make", "install"
   end
 
   test do
